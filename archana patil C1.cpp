22ab64ff434e52534aadf504a2deb807b168a66a
Archu21-lab <archupatil5958@gmail.com>
2025-11-22 21:47:04 +0530

    Add files via upload

---
+++ archana patil C1.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+ void main ()
+{
+	printf("name: archana patil");
+	printf("education: Btech/n");
+	printf("phone: 8010192338 /n");
+	printf("hobby: learning /n");
+	printf("course: full stack /n");
+	printf("email: pa359048gmail.com /n");	
+}
+
